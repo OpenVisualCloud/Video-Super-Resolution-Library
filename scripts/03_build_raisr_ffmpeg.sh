09cc58e8178a4409fea63b51cedc726b1b102c8d
Milosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-09-17 04:53:47 +0000

    Fix for build system to verify builds
    Fix for build system to verify builds
    
    Signed-off-by: Milosz Linkiewicz <milosz.linkiewicz@intel.com>

--- scripts/03_build_raisr_ffmpeg.sh
+++ scripts/03_build_raisr_ffmpeg.sh
@@ -25,7 +25,7 @@ cp ../Video-Super-Resolution-Library/ffmpeg/vf_raisr.c libavfilter/
 
 ./configure \
  --enable-libipp \
- --extra-cflags="-fopenmp" \
+ --extra-cflags="-fopenmp -I/opt/intel/oneapi/ipp/latest/include/ipp" \
  --extra-ldflags=-fopenmp \
  --enable-gpl \
  --enable-libx264 \
@@ -37,8 +37,10 @@ make -j $(nproc)
 
 cp -r ../Video-Super-Resolution-Library/filters* .
 
-echo -e "To run the library you can do the following: \n
+echo -e 'To run the library you can do the following: \n
       cd raisr/ffmpeg \n
       ./ffmpeg -y -i /input_files/input.mp4 -vf raisr=threadcount=20 -pix_fmt yuv420p /output_files/out.yuv \n
-And you can see more use cases in README file of Video-Super-Resolution-Library."
+And you can see more use cases in README file of Video-Super-Resolution-Library.\n\n
+Notice: If you get "ffmpeg: error while loading shared libraries", try first doing:
+export LD_LIBRARY_PATH="/opt/intel/oneapi/ipp/latest/lib/intel64:/usr/local/lib:${LD_LIBRARY_PATH}"'
 
