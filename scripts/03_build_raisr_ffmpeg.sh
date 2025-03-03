d94daed271312140a87a1cfaf4053d875eae00c2
Miłosz Linkiewicz <milosz.linkiewicz@intel.com>
2025-03-03 10:58:01 +0100

    Update 03_build_raisr_ffmpeg.sh
    Add patch apply using `patch` command instead of trouble causing `git`
    
    Signed-off-by: Miłosz Linkiewicz <milosz.linkiewicz@intel.com>

--- scripts/03_build_raisr_ffmpeg.sh
+++ scripts/03_build_raisr_ffmpeg.sh
@@ -31,6 +31,10 @@ pushd "${raisr_path}"
 sudo -E ./build.sh
 popd
 
+# TO-DO: Remove patch apply from bellow lines:
+# Apply a temporary patch - this will be removed after version of FFmpeg gets updated
+patch -p1 -i "${REPOSITORY_DIR}/scripts/patch/ffmpeg/0001-avcodec-x86-mathops-clip-constants.patch"
+
 # build ffmpeg
 pushd "${raisr_path}/../ffmpeg"
 cp "${raisr_path}/ffmpeg/vf_raisr.c" libavfilter/
